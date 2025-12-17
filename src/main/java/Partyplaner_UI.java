6dc82bdafe5b7af1230221bf060ebe96639c4b10
oezlemcengiz1 <oezlem.cengiz1@outlook.de>
2025-12-17 20:29:27 +0100

    add gui tools

--- src/main/java/Partyplaner_UI.java
+++ src/main/java/Partyplaner_UI.java
@@ -2,6 +2,24 @@ import javax.swing.*;
 
 public class Partyplaner_UI {
     private JPanel hauptPanel;
-    private JTextField partyplaner;
-    private JLabel partyplanerJLabel;
+    private JLabel partyplanerLabel;
+    private JLabel locationLabel;
+    private JComboBox locationcomboBox;
+    private JLabel musikDJLabel;
+    private JRadioButton rnbHiphopRadioButton;
+    private JRadioButton technoUndElectroRadioButton;
+    private JRadioButton afroBeatsRadioButton;
+    private JRadioButton rapRadioButton;
+    private JLabel essenLabel;
+    private JCheckBox essenJaCheckBox;
+    private JCheckBox essenNeinCheckBox;
+    private JComboBox essenComboBox;
+    private JLabel personenanzahlJLabel;
+    private JTextField PersonenanzahlTextField;
+    private JLabel telNrJLabel;
+    private JTextField telNrTextField;
+    private JLabel dekorationJLabel;
+    private JRadioButton blumenstilRadioButton;
+    private JRadioButton landhausstilRadioButton;
+    private JRadioButton gerlandeRadioButton;
 }
